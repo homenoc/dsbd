5c4a5f536e4ca97ed6cdc857e2e021b7c2e46083
yoneyan <contact@yoneyan.net>
2024-09-09 02:02:18 +0900

    #43 リファクタリング

--- service/views.py
+++ service/views.py
@@ -3,16 +3,15 @@ from django.core.paginator import EmptyPage, InvalidPage, Paginator
 from django.db.models import Q
 from django.shortcuts import render
 
-from custom_auth.models import UserGroup
 from service.models import Service
 
 
 @login_required
 def index(request):
-    user_groups = UserGroup.objects.filter(user=request.user)
+    group_ids = list(request.user.groups.values_list("id", flat=True))
     q = Q()
-    for user_group in user_groups:
-        q.add(Q(group=user_group.group), Q.OR)
+    for group_id in group_ids:
+        q.add(Q(group=group_id), Q.OR)
     services = Service.objects.filter(is_active=True).filter(q).order_by("id")
     per_page = int(request.GET.get("per_page", "20"))
     page = int(request.GET.get("page", "1"))
